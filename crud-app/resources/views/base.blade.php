ee6c854c7e1296274f315499e690bdc622fc2667
Willian Alves <wiilliian.alves@gmail.com>
2020-02-12 10:04:04 -0400

    build project for CSS start

--- crud-app/resources/views/base.blade.php
+++ crud-app/resources/views/base.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Laravel 6 & MySQL CRUD Tutorial</title>
+  <title>Teste prático - Desenvolvedor JR</title>
   <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
 </head>
 <body>
--- crud-app/routes/web.php
+++ crud-app/routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', function () {
 });
 
 Route::resource('usersdatas', 'UsersDataController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
