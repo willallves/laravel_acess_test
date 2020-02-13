3252caa02a5081dc93da0c9603f2688c40138a0a
Willian Alves <wiilliian.alves@gmail.com>
2020-02-12 20:57:32 -0400

    Added search by CEP to update

--- crud-app/resources/views/usersdata/edit.blade.php
+++ crud-app/resources/views/usersdata/edit.blade.php
@@ -1,3 +1,70 @@
+<!-- Adicionando Javascript -->
+<script type="text/javascript" >
+    
+    function limpa_formulário_cep() {
+            //Limpa valores do formulário de cep.
+            document.getElementById('address').value=("");
+            document.getElementById('city').value=("");
+            document.getElementById('state').value=("");
+    }
+
+    function meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            //Atualiza os campos com os valores.
+            document.getElementById('address').value=(conteudo.logradouro)+ " " + (conteudo.bairro);
+            document.getElementById('city').value=(conteudo.localidade);
+            document.getElementById('state').value=(conteudo.uf);
+        } //end if.
+        else {
+            //CEP não Encontrado.
+            limpa_formulário_cep();
+            alert("CEP não encontrado.");
+        }
+    }
+        
+    function pesquisacep(valor) {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = valor.replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                document.getElementById('address').value="...";
+                document.getElementById('city').value="...";
+                document.getElementById('state').value="...";
+                
+                //Cria um elemento javascript.
+                var script = document.createElement('script');
+
+                //Sincroniza com o callback.
+                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+                //Insere script no documento e carrega o conteúdo.
+                document.body.appendChild(script);
+
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    };
+
+    </script>
+
 @extends('base') 
 @section('main')
 <div class="row">
@@ -47,22 +114,22 @@
 
             <div class="form-group">
                 <label for="address">Endereco:</label>
-                <input type="text" class="form-control" name="address" value={{ $usersdata->address }} />
+                <input type="text" class="form-control" id="address" name="address" value={{ $usersdata->address }} />
             </div>
 
             <div class="form-group">
                 <label for="city">Cidade:</label>
-                <input type="text" class="form-control" name="city" value={{ $usersdata->city }} />
+                <input type="text" class="form-control" id="city" name="city" value={{ $usersdata->city }} />
             </div>
 
             <div class="form-group">
                 <label for="state">Estado:</label>
-                <input type="text" class="form-control" name="state" value={{ $usersdata->state }} />
+                <input type="text" class="form-control" id="state" name="state" value={{ $usersdata->state }} />
             </div>
 
             <div class="form-group">
                 <label for="Zip_code">CEP:</label>
-                <input type="text" class="form-control" name="Zip_code" value={{ $usersdata->Zip_code }} />
+                <input type="text" class="form-control" id="Zip_code" name="Zip_code" onblur="pesquisacep(this.value);" value={{ $usersdata->Zip_code }} />
             </div>
 
 
